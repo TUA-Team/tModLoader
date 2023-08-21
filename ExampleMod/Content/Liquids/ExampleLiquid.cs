75d0ec026468733894bd68e712b799beb4276218
mysteroman <mysteroman.minecraft@gmail.com>
2023-08-21 11:54:09 -0400

    Added example hooks to ExampleLiquid

--- ExampleMod/Content/Liquids/ExampleLiquid.cs
+++ ExampleMod/Content/Liquids/ExampleLiquid.cs
@@ -16,5 +16,19 @@ namespace ExampleMod.Content.Liquids
 			WaterfallLength = 15;
 			DefaultOpacity = 1;
 		}
+
+		public override void OnItemCollide(Item item) {
+			
+		}
+
+		public override bool HasWetItemPhysics(Item item) {
+			return true;
+		}
+
+		public override void WetItemPhysics(Item item, Vector2 velocity, ref float gravity, ref float maxFallSpeed, ref Vector2 wetVelocity) {
+			wetVelocity = velocity;
+			gravity = -1f;
+			maxFallSpeed = 5f;
+		}
 	}
 }
